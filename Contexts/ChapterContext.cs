b1e123de5bd3c60ef3a8d639123816165d34548c
1000tonaugusto <miltonaugustodesouza@gmail.com>
2022-06-05 18:14:50 -0300

    Primeiro Commit

---
+++ Contexts/ChapterContext.cs
@@ -0,0 +1,31 @@
+﻿using Chapter1000ton.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter1000ton.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+
+        // Vamos utilizar esse metodo para configurar o banco de dados
+        protected override void
+
+    OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = MILTON\\SQLEXPRESS; Initial catalog=Chapter; Integrated Security=true");
+            }
+        }
+
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ Controllers/LivroController.cs
@@ -0,0 +1,37 @@
+﻿using Chapter1000ton.Models;
+using Chapter1000ton.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter1000ton.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+    }
+}
