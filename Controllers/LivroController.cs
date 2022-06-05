6ce7d69a10a46a565d5e158a0a7fc8f0dc8d734d
1000tonaugusto <miltonaugustodesouza@gmail.com>
2022-06-05 19:46:55 -0300

    Primeiro commit

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -33,5 +33,67 @@ namespace Chapter1000ton.Controllers
             }
         }
 
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livro = _livroRepository.BuscarPorId(id);
+
+                if (livro == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(livro);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Inlcuir(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Incluir(livro);
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Alterar(int id, Livro livro)
+        {
+            try
+            {
+                _livroRepository.Alterar(id, livro);
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
     }
 }
--- Repositories/LivroRepository.cs
+++ Repositories/LivroRepository.cs
@@ -17,6 +17,44 @@ namespace Chapter1000ton.Repositories
             return _context.Livros.ToList();
         }
 
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Incluir(Livro livro)
+        {
+            _context.Livros.Add(livro);
+
+            _context.SaveChanges();
+        }
+
+        public void Alterar(int id, Livro livro)
+        {
+            Livro livroBuscado = _context.Livros.Find(id);
+
+            if (livroBuscado != null)
+            {
+                livroBuscado.Titulo = livro.Titulo;
+
+                livroBuscado.QuantidadePaginas = livro.QuantidadePaginas;
+
+                livro.Disponivel = livro.Disponivel;
+            }
+            _context.Livros.Update(livroBuscado);
+
+            _context.SaveChanges();
+
+        }
+
+        public void Deletar(int id)
+        {
+            Livro livro = _context.Livros.Find(id);
+
+            _context.Livros.Remove(livro);
+
+            _context.SaveChanges();
+        }
 
 
     }
